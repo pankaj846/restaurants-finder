87354d98c8b4a2b7c5460ea771eca088a6de8fbe
pankaj <pkstark4@gmail.com>
2022-02-27 15:23:38 +0530

    Updates

---
+++ precache-manifest.23bc9412be0bc2924fd38e912c60a9e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d63229f3dd0456dab2f1fbb6abadf5f",
+    "url": "/projects/restaurants-finder/index.html"
+  },
+  {
+    "revision": "ae4de8c3eada4c847da5",
+    "url": "/projects/restaurants-finder/static/css/main.365ec8f8.chunk.css"
+  },
+  {
+    "revision": "8d53dadcb151375df612",
+    "url": "/projects/restaurants-finder/static/js/2.01d360e3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/projects/restaurants-finder/static/js/2.01d360e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae4de8c3eada4c847da5",
+    "url": "/projects/restaurants-finder/static/js/main.d5e8e4ed.chunk.js"
+  },
+  {
+    "revision": "83106fe21790a9d3f0d7",
+    "url": "/projects/restaurants-finder/static/js/runtime-main.f67b3c3f.js"
+  },
+  {
+    "revision": "5639812013b1fc90b099740731917bb1",
+    "url": "/projects/restaurants-finder/static/media/Restaurant-Food.56398120.jpg"
+  }
+]);
